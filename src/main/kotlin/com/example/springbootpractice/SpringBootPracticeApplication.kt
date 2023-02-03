35a9a290ccdc12392d3a9327f0a6b76ccf67f605
junho3 <chojunho433@gmail.com>
2023-02-04 00:24:56 +0900

    DispatcherServlet으로 변경

--- src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
+++ src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
@@ -2,19 +2,14 @@ package com.example.springbootpractice
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.ServletContextInitializer
-import org.springframework.context.support.GenericApplicationContext
 import org.springframework.context.support.registerBean
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import org.springframework.web.context.support.GenericWebApplicationContext
+import org.springframework.web.servlet.DispatcherServlet
 
 class SpringBootPracticeApplication
 
 fun main(args: Array<String>) {
-    val applicationContext = GenericApplicationContext().apply {
+    val applicationContext = GenericWebApplicationContext().apply {
         registerBean<HelloController>()
         registerBean<SimpleHelloService>()
         refresh()
@@ -24,22 +19,8 @@ fun main(args: Array<String>) {
     val webServer = serverFactory.getWebServer(
         ServletContextInitializer { servletContext ->
             servletContext.addServlet(
-                "frontcontroller",
-                object : HttpServlet() {
-                    override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
-                        if (req.requestURI.equals("/hello") && req.method.equals(HttpMethod.GET.name)) {
-                            val name: String? = req.getParameter("name")
-
-                            val helloController = applicationContext.getBean(HelloController::class.java)
-                            val result = helloController.hello(name)
-
-                            resp.contentType = MediaType.TEXT_PLAIN_VALUE
-                            resp.writer.println(result)
-                        } else {
-                            resp.status = HttpStatus.NOT_FOUND.value()
-                        }
-                    }
-                }
+                "dispatcherServlet",
+                DispatcherServlet(applicationContext)
             ).addMapping("/*")
         }
     )
