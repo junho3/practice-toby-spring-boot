3893e7e01f24551576b861dd2128433afffa9d7f
junho3 <chojunho433@gmail.com>
2023-02-03 22:55:39 +0900

    status, headers 상수로 변경

--- src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
+++ src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
@@ -2,6 +2,9 @@ package com.example.springbootpractice
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.ServletContextInitializer
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -16,8 +19,8 @@ fun main(args: Array<String>) {
                 "hello",
                 object : HttpServlet() {
                     override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
-                        resp.status = 200
-                        resp.setHeader("Content-Type", "text/plain")
+                        resp.status = HttpStatus.OK.value()
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE)
                         resp.writer.println("Hello Servlet")
                     }
                 }
