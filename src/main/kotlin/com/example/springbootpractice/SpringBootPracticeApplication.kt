ffb0ec0e1b63c5127024361dc789f66379033282
junho3 <chojunho433@gmail.com>
2023-02-03 22:53:12 +0900

    서블릿 등록

--- src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
+++ src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
@@ -1,11 +1,29 @@
 package com.example.springbootpractice
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.ServletContextInitializer
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 class SpringBootPracticeApplication
 
 fun main(args: Array<String>) {
     val serverFactory = TomcatServletWebServerFactory()
-    val webServer = serverFactory.getWebServer()
+    val webServer = serverFactory.getWebServer(
+        ServletContextInitializer { servletContext ->
+            servletContext.addServlet(
+                "hello",
+                object : HttpServlet() {
+                    override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
+                        resp.status = 200
+                        resp.setHeader("Content-Type", "text/plain")
+                        resp.writer.println("Hello Servlet")
+                    }
+                }
+            ).addMapping("/hello")
+        }
+    )
+
     webServer.start()
 }
