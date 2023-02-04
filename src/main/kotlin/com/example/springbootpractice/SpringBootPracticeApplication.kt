ba5c6deaa82f668cd54d0726794720ff696562b2
junho3 <chojunho433@gmail.com>
2023-02-04 11:42:17 +0900

    스프링컨테이너 기동할 때 Bean 등록하도록 변경

--- src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
+++ src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
@@ -2,28 +2,34 @@ package com.example.springbootpractice
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.ServletContextInitializer
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.support.registerBean
 import org.springframework.web.context.support.GenericWebApplicationContext
 import org.springframework.web.servlet.DispatcherServlet
+import javax.servlet.ServletContext
 
 class SpringBootPracticeApplication
 
 fun main(args: Array<String>) {
-    val applicationContext = GenericWebApplicationContext().apply {
-        registerBean<HelloController>()
-        registerBean<SimpleHelloService>()
-        refresh()
-    }
-
-    val serverFactory = TomcatServletWebServerFactory()
-    val webServer = serverFactory.getWebServer(
-        ServletContextInitializer { servletContext ->
-            servletContext.addServlet(
-                "dispatcherServlet",
-                DispatcherServlet(applicationContext)
-            ).addMapping("/*")
+    val applicationContext: GenericWebApplicationContext = object : GenericWebApplicationContext() {
+        override fun onRefresh() {
+            super.onRefresh()
+            val serverFactory: ServletWebServerFactory = TomcatServletWebServerFactory()
+            val webServer = serverFactory.getWebServer(
+                ServletContextInitializer { servletContext: ServletContext ->
+                    servletContext.addServlet(
+                        "dispatcherServlet",
+                        DispatcherServlet(this)
+                    )
+                        .addMapping("/*")
+                }
+            )
+            webServer.start()
         }
-    )
-
-    webServer.start()
+    }
+        .apply {
+            registerBean<HelloController>()
+            registerBean<SimpleHelloService>()
+            refresh()
+        }
 }
