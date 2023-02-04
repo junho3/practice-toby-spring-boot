0081e7c11d928d4c189d8339624c57a7643d4c51
junho3 <chojunho433@gmail.com>
2023-02-04 13:39:59 +0900

    기존 applicationContext 생성하는 코드 주석 처리 Accidental override로 인하여 어플리케이션 기동이 안됨

--- src/main/kotlin/com/example/springbootpractice/MySpringApplication.kt
+++ src/main/kotlin/com/example/springbootpractice/MySpringApplication.kt
@@ -9,35 +9,35 @@ import javax.servlet.ServletContext
 class MySpringApplication {
     companion object {
         fun run(applicationClass: Class<*>, vararg args: String) {
-            val applicationContext: AnnotationConfigWebApplicationContext =
-                object : AnnotationConfigWebApplicationContext() {
-                    override fun setClassLoader(classLoader: ClassLoader) {
-                        // TODO Accidental override: The following declarations have the same JVM signature 발생 해결방법을 찾지 못 함
-                        super.setClassLoader(classLoader)
-                    }
-
-                    override fun onRefresh() {
-                        super.onRefresh()
-                        val serverFactory: ServletWebServerFactory = getBean(ServletWebServerFactory::class.java)
-                        val dispatcherServlet: DispatcherServlet = getBean(DispatcherServlet::class.java)
-                        dispatcherServlet.setApplicationContext(this)
-
-                        val webServer = serverFactory.getWebServer(
-                            ServletContextInitializer { servletContext: ServletContext ->
-                                servletContext.addServlet(
-                                    "dispatcherServlet",
-                                    dispatcherServlet
-                                )
-                                    .addMapping("/*")
-                            }
-                        )
-                        webServer.start()
-                    }
-                }
-                    .apply {
-                        register(applicationClass)
-                        refresh()
-                    }
+//            val applicationContext: AnnotationConfigWebApplicationContext =
+//                object : AnnotationConfigWebApplicationContext() {
+//                    override fun setClassLoader(classLoader: ClassLoader) {
+//                        // TODO Accidental override: The following declarations have the same JVM signature 발생 해결방법을 찾지 못 함
+//                        super.setClassLoader(classLoader)
+//                    }
+//
+//                    override fun onRefresh() {
+//                        super.onRefresh()
+//                        val serverFactory: ServletWebServerFactory = getBean(ServletWebServerFactory::class.java)
+//                        val dispatcherServlet: DispatcherServlet = getBean(DispatcherServlet::class.java)
+//                        dispatcherServlet.setApplicationContext(this)
+//
+//                        val webServer = serverFactory.getWebServer(
+//                            ServletContextInitializer { servletContext: ServletContext ->
+//                                servletContext.addServlet(
+//                                    "dispatcherServlet",
+//                                    dispatcherServlet
+//                                )
+//                                    .addMapping("/*")
+//                            }
+//                        )
+//                        webServer.start()
+//                    }
+//                }
+//                    .apply {
+//                        register(applicationClass)
+//                        refresh()
+//                    }
         }
     }
 }
