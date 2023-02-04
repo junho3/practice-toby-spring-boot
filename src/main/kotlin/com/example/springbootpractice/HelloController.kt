e7abe5d9daf96ce3525395e6209380fdc0c1e2b2
junho3 <chojunho433@gmail.com>
2023-02-04 11:29:04 +0900

    @RequestMapping, @ResponseBody 어노테이션 추가

--- src/main/kotlin/com/example/springbootpractice/HelloController.kt
+++ src/main/kotlin/com/example/springbootpractice/HelloController.kt
@@ -1,6 +1,17 @@
 package com.example.springbootpractice
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@RequestMapping("/hello")
 class HelloController(private val helloService: HelloService) {
+    /**
+     * @ResponseBody가 없으면 리턴 타입이 String인 경우, DispatchServlet은 기본적으로 view(jsp)를 찾으려고 함. view가 없으니 404 에러를 리턴함
+     * @ResponseBody를 추가하면, content-type: text/plain 를 리턴하기 때문에 view를 찾지 않고 문자열로 리턴함
+     */
+    @GetMapping
+    @ResponseBody
     fun hello(name: String?): String {
         return helloService.sayHello(requireNotNull(name))
     }
