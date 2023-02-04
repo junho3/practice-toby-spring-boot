299e76f3c64eea24070747d34fba0a531bb42119
junho3 <chojunho433@gmail.com>
2023-02-04 14:02:01 +0900

    테스트 코드 추가

---
+++ src/test/kotlin/com/example/springbootpractice/HelloApiTest.kt
@@ -0,0 +1,24 @@
+package com.example.springbootpractice
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+
+class HelloApiTest {
+    @Test
+    fun helloApi() {
+        val restTemplate = TestRestTemplate()
+        val parameter = "Spring"
+
+        val response: ResponseEntity<String> =
+            restTemplate.getForEntity("http://localhost:8080/hello?name={name}", String::class.java, parameter)
+
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response.headers.getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE)
+        assertThat(response.body).isEqualTo("Hello $parameter")
+    }
+}
