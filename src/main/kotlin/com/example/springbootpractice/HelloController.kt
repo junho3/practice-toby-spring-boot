d46b3b96a61c75ab2af3c8e759a3677435e0ba16
junho3 <chojunho433@gmail.com>
2023-02-04 22:20:44 +0900

    applicationContext 삭제

--- src/main/kotlin/com/example/springbootpractice/HelloController.kt
+++ src/main/kotlin/com/example/springbootpractice/HelloController.kt
@@ -1,13 +1,11 @@
 package com.example.springbootpractice
 
-import org.springframework.context.ApplicationContext
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController(
-    private val helloService: HelloService,
-    private val applicationContext: ApplicationContext
+    private val helloService: HelloService
 ) {
     /**
      * @ResponseBody가 없으면 리턴 타입이 String인 경우, DispatchServlet은 기본적으로 view(jsp)를 찾으려고 함. view가 없으니 404 에러를 리턴함
