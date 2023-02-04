a8230a77f101b5a969c177bccbf4dac46d7cff71
junho3 <chojunho433@gmail.com>
2023-02-04 12:52:47 +0900

    AnnotationConfigWebApplicationContext로 변경 kotlin에서는 setClassLoader 메소드를 오버라이드 하라고 나옴 override를 하면 Accidental override 에러 발생

--- src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
+++ src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
@@ -3,15 +3,23 @@ package com.example.springbootpractice
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.ServletContextInitializer
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
-import org.springframework.context.support.registerBean
-import org.springframework.web.context.support.GenericWebApplicationContext
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
 import org.springframework.web.servlet.DispatcherServlet
 import javax.servlet.ServletContext
 
+@ComponentScan
+@Configuration
 class SpringBootPracticeApplication
 
 fun main(args: Array<String>) {
-    val applicationContext: GenericWebApplicationContext = object : GenericWebApplicationContext() {
+    val applicationContext: AnnotationConfigWebApplicationContext = object : AnnotationConfigWebApplicationContext() {
+        override fun setClassLoader(classLoader: ClassLoader) {
+            // TODO Accidental override: The following declarations have the same JVM signature 발생 해결방법을 찾지 못 함
+            super.setClassLoader(classLoader)
+        }
+
         override fun onRefresh() {
             super.onRefresh()
             val serverFactory: ServletWebServerFactory = TomcatServletWebServerFactory()
@@ -28,8 +36,7 @@ fun main(args: Array<String>) {
         }
     }
         .apply {
-            registerBean<HelloController>()
-            registerBean<SimpleHelloService>()
+            register(SpringBootPracticeApplication::class.java)
             refresh()
         }
 }
