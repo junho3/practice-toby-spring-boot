1d304df102fa3c65f1faa2ae8480bf89d3b1c73b
junho3 <chojunho433@gmail.com>
2023-02-04 12:53:48 +0900

    스테레오 타입 어노테이션으로 변경

--- src/main/kotlin/com/example/springbootpractice/SimpleHelloService.kt
+++ src/main/kotlin/com/example/springbootpractice/SimpleHelloService.kt
@@ -1,5 +1,8 @@
 package com.example.springbootpractice
 
+import org.springframework.stereotype.Service
+
+@Service
 class SimpleHelloService : HelloService {
     override fun sayHello(name: String): String {
         return "Hello $name"
