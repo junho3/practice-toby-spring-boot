ccf75fce8c8ada4bbc849c309e049009b22135dd
junho3 <chojunho433@gmail.com>
2023-02-06 22:25:50 +0900

    config 빈을 동적으로 할당하기 위한 selectImports 구현

--- src/main/kotlin/com/example/config/EnableAutoConfiguration.kt
+++ src/main/kotlin/com/example/config/EnableAutoConfiguration.kt
@@ -1,10 +1,8 @@
 package com.example.config
 
-import com.example.config.autoconfig.DispatcherServletConfig
-import com.example.config.autoconfig.TomcatWebServerConfig
 import org.springframework.context.annotation.Import
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-@Import(DispatcherServletConfig::class, TomcatWebServerConfig::class)
+@Import(MyConfigImportSelector::class)
 annotation class EnableAutoConfiguration()
