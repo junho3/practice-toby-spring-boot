e62f4996865beb78eecddb7c148a0aaa159cc283
junho3 <chojunho433@gmail.com>
2023-02-06 22:07:08 +0900

    Config Import 추가

--- src/main/kotlin/com/example/springbootpractice/MyComponent.kt
+++ src/main/kotlin/com/example/springbootpractice/MyComponent.kt
@@ -1,8 +1,11 @@
 package com.example.springbootpractice
 
+import com.example.config.Config
+import org.springframework.context.annotation.Import
 import org.springframework.stereotype.Component
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
 @Component
+@Import(Config::class)
 annotation class MyComponent()
