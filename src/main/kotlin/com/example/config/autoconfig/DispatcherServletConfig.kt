56f74b880060a0dedf20171cdd464cd05ee0da34
junho3 <chojunho433@gmail.com>
2023-02-07 22:06:42 +0900

    Config 클래스의 어노테이션을 @MyAutoConfiguration으로 변경 proxyBeanMethods = false 선언

--- src/main/kotlin/com/example/config/autoconfig/DispatcherServletConfig.kt
+++ src/main/kotlin/com/example/config/autoconfig/DispatcherServletConfig.kt
@@ -1,10 +1,12 @@
 package com.example.config.autoconfig
 
+import com.example.config.MyAutoConfiguration
+import com.example.config.MySpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.DispatcherServlet
 
-@Configuration
+@MyAutoConfiguration
 class DispatcherServletConfig {
     @Bean
     fun dispatcherServlet(): DispatcherServlet {
--- src/main/kotlin/com/example/config/autoconfig/TomcatWebServerConfig.kt
+++ src/main/kotlin/com/example/config/autoconfig/TomcatWebServerConfig.kt
@@ -1,11 +1,13 @@
 package com.example.config.autoconfig
 
+import com.example.config.MyAutoConfiguration
+import com.example.config.MySpringBootApplication
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@Configuration
+@MyAutoConfiguration
 class TomcatWebServerConfig {
     @Bean
     fun servletWebServerFactory(): ServletWebServerFactory {
