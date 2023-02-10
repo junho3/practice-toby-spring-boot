1626c9d0a48993e727c9c82025d1441f72670eb2
junho3 <chojunho433@gmail.com>
2023-02-10 23:01:46 +0900

    jetty 웹서버 관련 설정 추가

--- src/main/kotlin/com/example/config/autoconfig/JettyWebServerConfig.kt
+++ src/main/kotlin/com/example/config/autoconfig/JettyWebServerConfig.kt
@@ -2,15 +2,16 @@ package com.example.config.autoconfig
 
 import com.example.config.MyAutoConfiguration
 import com.example.config.MySpringBootApplication
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @MyAutoConfiguration
-class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
+class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     fun servletWebServerFactory(): ServletWebServerFactory {
-        return TomcatServletWebServerFactory()
+        return JettyServletWebServerFactory()
     }
 }
