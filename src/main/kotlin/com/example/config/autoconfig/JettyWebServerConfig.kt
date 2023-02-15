472de50c06e4eb2d85b72fff3d9e3b98bab6f05c
junho3 <chojunho433@gmail.com>
2023-02-15 21:33:34 +0900

    @ConditionalOnMissingBean 어노테이션을 추가하여, 동일한 타입의 빈이 있는지 체크 빈이 존재하지 않으면, @ConditionalOnMissingBean을 선언한 @Bean을 Context에 올림

--- src/main/kotlin/com/example/config/autoconfig/JettyWebServerConfig.kt
+++ src/main/kotlin/com/example/config/autoconfig/JettyWebServerConfig.kt
@@ -2,6 +2,7 @@ package com.example.config.autoconfig
 
 import com.example.config.ConditionalMyOnClass
 import com.example.config.MyAutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
@@ -16,6 +17,7 @@ import org.springframework.util.ClassUtils
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
+    @ConditionalOnMissingBean
     fun servletWebServerFactory(): ServletWebServerFactory {
         return JettyServletWebServerFactory()
     }
