de305acac9214099e9c98a7b5f121529424ea700
junho3 <chojunho433@gmail.com>
2023-02-17 23:09:33 +0900

    applicationRunner를 추가하여 앱이 기동할 때 application.properties 값을 읽어오도록 Bean 추가

--- src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
+++ src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
@@ -1,10 +1,22 @@
 package com.example.springbootpractice
 
 import com.example.config.MySpringBootApplication
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.env.Environment
 
 @MySpringBootApplication
-class SpringBootPracticeApplication
+class SpringBootPracticeApplication {
+
+    @Bean
+    fun applicationRunner(env: Environment): ApplicationRunner {
+        return ApplicationRunner {
+            val name = env.getProperty("my.name")
+            println("name : $name")
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(SpringBootPracticeApplication::class.java, *args)
