03bcd5e75430793cebba7ff968c584ce2aaf1d9b
junho3 <chojunho433@gmail.com>
2023-02-20 22:42:02 +0900

    타입힌트 추가

--- src/main/kotlin/com/example/config/EnableMyConfigurationProperties.kt
+++ src/main/kotlin/com/example/config/EnableMyConfigurationProperties.kt
@@ -1,10 +1,9 @@
 package com.example.config
 
-import com.example.config.autoconfig.ServerProperties
 import org.springframework.context.annotation.Import
 import kotlin.reflect.KClass
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
 @Import(MyConfigurationPropertiesImportSelector::class)
-annotation class EnableMyConfigurationProperties(val value: KClass<ServerProperties>)
+annotation class EnableMyConfigurationProperties(val value: KClass<*>)
