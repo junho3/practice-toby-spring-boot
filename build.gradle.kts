5878825c1361185df3db5f06a64b55de9c1c596b
junho3 <chojunho433@gmail.com>
2023-02-20 22:10:55 +0900

    noArg 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.noarg") version "1.6.21"
 }
 
 group = "com.example"
@@ -24,6 +25,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+noArg {
+    annotation("org.springframework.stereotype.Component")
 }
 
 tasks.withType<KotlinCompile> {
