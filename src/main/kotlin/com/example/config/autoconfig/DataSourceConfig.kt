41556ab765292cc9f2baa23a9ecf3f7c5e53ca19
junho3 <chojunho433@gmail.com>
2023-02-23 22:35:17 +0900

    jdbcTemplate과 jdbcTransactionManager Bean 설정 추가

--- src/main/kotlin/com/example/config/autoconfig/DataSourceConfig.kt
+++ src/main/kotlin/com/example/config/autoconfig/DataSourceConfig.kt
@@ -5,14 +5,19 @@ import com.example.config.EnableMyConfigurationProperties
 import com.example.config.MyAutoConfiguration
 import com.zaxxer.hikari.HikariDataSource
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate
 import org.springframework.context.annotation.Bean
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.datasource.SimpleDriverDataSource
+import org.springframework.jdbc.support.JdbcTransactionManager
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import java.sql.Driver
 import javax.sql.DataSource
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties::class)
+@EnableTransactionManagement
 class DataSourceConfig {
     @Bean
     @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
@@ -41,5 +46,17 @@ class DataSourceConfig {
         return dataSource
     }
 
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnSingleCandidate(DataSource::class) // DataSource Bean이 1개만 존재할 때 해당 Bean을 가져와서 사용하겠다라는 의미
+    fun jdbcTemplate(dataSource: DataSource): JdbcTemplate {
+        return JdbcTemplate(dataSource)
+    }
 
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnSingleCandidate(DataSource::class)
+    fun jdbcTransactionManager(dataSource: DataSource): JdbcTransactionManager {
+        return JdbcTransactionManager(dataSource)
+    }
 }
