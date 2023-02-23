a838a16fec8251d802ca5a140400ce156e1f7bb6
junho3 <chojunho433@gmail.com>
2023-02-23 22:38:14 +0900

    IntegrationTest 어노테이션 추가

--- src/test/kotlin/com/example/springbootpractice/IntegrationTest.kt
+++ src/test/kotlin/com/example/springbootpractice/IntegrationTest.kt
@@ -1,21 +1,15 @@
 package com.example.springbootpractice
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.transaction.annotation.Transactional
-import javax.sql.DataSource
 
-@IntegrationTest
-class DataSourceTest {
-    @Autowired
-    lateinit var dataSource: DataSource
-    @Test
-    fun connect() {
-        val connection = dataSource.connection
-        connection.close()
-    }
-}
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@ExtendWith(SpringExtension::class)
+@ContextConfiguration(classes = [SpringBootPracticeApplication::class])
+@TestPropertySource("classpath:/application.properties")
+@Transactional
+annotation class IntegrationTest()
