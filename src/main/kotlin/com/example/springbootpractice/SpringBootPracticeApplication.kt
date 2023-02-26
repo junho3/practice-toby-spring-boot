e0ee584e6d57682b24c5f3ea5158c72f7503cf07
junho3 <chojunho433@gmail.com>
2023-02-26 11:39:53 +0900

    어플리케이션이 기동할 때 hello 테이블 저장하도록 추가

--- src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
+++ src/main/kotlin/com/example/springbootpractice/SpringBootPracticeApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.core.env.Environment
+import org.springframework.jdbc.core.JdbcTemplate
 
 @MySpringBootApplication
 class SpringBootPracticeApplication {
@@ -19,6 +20,9 @@ class SpringBootPracticeApplication {
 }
 
 fun main(args: Array<String>) {
-    SpringApplication.run(SpringBootPracticeApplication::class.java, *args)
+    val context = SpringApplication.run(SpringBootPracticeApplication::class.java, *args)
 //    MySpringApplication.run(SpringBootPracticeApplication::class.java, *args)
+
+    val jdbcTemplate = context.getBean(JdbcTemplate::class.java)
+    jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS hello(name varchar(50) primary key, count int)")
 }
