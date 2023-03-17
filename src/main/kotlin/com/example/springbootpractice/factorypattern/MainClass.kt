3703a0743d9c7e7d2e2aea8d2b8c4cc4dda7699a
junho3 <chojunho433@gmail.com>
2023-03-17 11:52:31 +0900

    라인정리

--- src/main/kotlin/com/example/springbootpractice/factorypattern/MainClass.kt
+++ src/main/kotlin/com/example/springbootpractice/factorypattern/MainClass.kt
@@ -7,8 +7,6 @@ abstract class MainClass {
     open fun approveKakaoPay(param: ApproveParam) {}
     open fun approveVirtualAccount(param: ApproveParam) {}
     open fun cancel(param: CancelParam) {}
-
     open fun partialCancel(param: CancelParam) {}
-
     open fun receipt(param: Boolean) {}
 }
